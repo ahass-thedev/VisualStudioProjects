dd2e83f1659c233a3287b010280577064d9e35b7
ahassanein99 <achmadoo@outlook.com>
2018-07-20 01:26:02 -0400

    Add files via upload

---
+++ picture/picture/Form1.Designer.vb
@@ -0,0 +1,113 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.picture.My.Resources.Resources._1509786_2
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(588, 392)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 410)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(113, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Big"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(131, 410)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(113, 23)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Increase"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(250, 410)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(113, 23)
+        Me.Button3.TabIndex = 3
+        Me.Button3.Text = "Decrease"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(369, 410)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(112, 23)
+        Me.Button4.TabIndex = 4
+        Me.Button4.Text = "Move"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(487, 410)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(112, 23)
+        Me.Button5.TabIndex = 5
+        Me.Button5.Text = "Default"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(615, 436)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "Form1"
+        Me.Text = "Picture"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+
+End Class
---
+++ picture/picture/Form1.vb
@@ -0,0 +1,51 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        PictureBox1.Size = New Size(500, 500)
+    End Sub
+
+    Private Sub Button2_Click(sender As System.Object, e As System.EventArgs) Handles Button2.Click
+        Dim x, y As Integer
+        x = PictureBox1.Width + 5
+        y = PictureBox1.Height + 5
+        PictureBox1.Size = New Size(x, y)
+    End Sub
+
+    Private Sub Button3_Click(sender As System.Object, e As System.EventArgs) Handles Button3.Click
+        Dim x, y As Integer
+        x = PictureBox1.Width - 5
+        y = PictureBox1.Height - 5
+        PictureBox1.Size = New Size(x, y)
+    End Sub
+
+    Private Sub Button4_Click(sender As System.Object, e As System.EventArgs) Handles Button4.Click
+        Dim x, y As Integer
+        Dim answer As String
+        answer = InputBox("Which direction do you want the picturebox to move?                                                                   your options are: left, right, up and down")
+        answer = answer.ToLower()
+        If answer = "up" Then
+            x = PictureBox1.Location.X + 0
+            y = PictureBox1.Location.Y - 10
+        ElseIf answer = "down" Then
+            x = PictureBox1.Location.X + 0
+            y = PictureBox1.Location.Y + 10
+        ElseIf answer = "right" Then
+            x = PictureBox1.Location.X + 10
+            y = PictureBox1.Location.Y + 0
+        ElseIf answer = "left" Then
+            x = PictureBox1.Location.X - 10
+            y = PictureBox1.Location.Y + 0
+        Else
+            MessageBox.Show("I told you the options, why didn't you choose one of those options?")
+        End If
+        PictureBox1.Location = New Point(x, y)
+    End Sub
+
+    Private Sub Button5_Click(sender As System.Object, e As System.EventArgs) Handles Button5.Click
+        Dim x, y As Integer
+        PictureBox1.Size = New Size(588, 392)
+        x = PictureBox1.Location.X
+        y = PictureBox1.Location.Y
+        PictureBox1.Location = New Point(12, 12)
+    End Sub
+End Class
