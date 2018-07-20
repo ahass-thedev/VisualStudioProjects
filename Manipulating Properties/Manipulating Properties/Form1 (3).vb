88b68eb64173202f061dd4aeeda3b11d45812021
ahassanein99 <achmadoo@outlook.com>
2018-07-20 01:18:15 -0400

    Add files via upload

---
+++ Manipulating Properties/Manipulating Properties/Form1 (3).vb
@@ -0,0 +1,48 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        Dim word As String
+        word = InputBox("Name the form")
+        Me.Text = word
+        MessageBox.Show("Changing the color")
+        Me.BackColor = Color.LightSkyBlue
+    End Sub
+
+    Private Sub Button2_Click(sender As System.Object, e As System.EventArgs) Handles Button2.Click
+        Label1.Text = ("The new label")
+        Label1.ForeColor = Color.Red
+        Label1.BackColor = Color.Black
+    End Sub
+
+    Private Sub Button3_Click(sender As System.Object, e As System.EventArgs) Handles Button3.Click
+        Button1.Hide()
+        Button2.Hide()
+        Button3.Hide()
+        Label1.Hide()
+    End Sub
+
+    Private Sub Button4_Click(sender As System.Object, e As System.EventArgs) Handles Button4.Click
+        Button1.Show()
+        Button2.Show()
+        Button3.Show()
+        Label1.Show()
+    End Sub
+
+    Private Sub Button5_Click(sender As System.Object, e As System.EventArgs) Handles Button5.Click
+        Dim speak As String
+        speak = InputBox("Name me")
+        Label1.Text = speak
+    End Sub
+
+    Private Sub Button6_Click(sender As System.Object, e As System.EventArgs) Handles Button6.Click
+        Label1.Text = ("Label 1")
+        Me.BackColor = DefaultBackColor
+        Me.ForeColor = DefaultForeColor
+        Label1.BackColor = DefaultBackColor
+        Label1.ForeColor = DefaultForeColor
+    End Sub
+
+    Private Sub Label1_Click(sender As System.Object, e As System.EventArgs) Handles Label1.Click
+
+    End Sub
+End Class
---
+++ Manipulating Properties/Manipulating Properties/My Project/AssemblyInfo (3).vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Manipulating Properties")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Wayne Board of Education")> 
+<Assembly: AssemblyProduct("Manipulating Properties")> 
+<Assembly: AssemblyCopyright("Copyright © Wayne Board of Education 2014")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("c8196b85-49f0-4040-81c2-57a44a8963d6")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
