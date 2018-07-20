d47cdf98dbc76924eeaa14e3c43aeb94b4e60408
ahassanein99 <achmadoo@outlook.com>
2018-07-20 01:01:32 -0400

    Add files via upload

---
+++ Decisions 2/Decisions 2/Form1 (2).vb
@@ -0,0 +1,131 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        Dim choice As String
+        choice = InputBox("What color do you want the label to be? Green, blue or red")
+        choice = choice.ToLower()
+        If choice = "blue" Then
+            Label1.Show()
+            MessageBox.Show("Ok, changing the color to blue")
+            Label1.ForeColor = Color.Blue
+        ElseIf choice = "green" Then
+            Label1.Show()
+            MessageBox.Show("Ok, changing the color to green")
+            Label1.ForeColor = Color.Green
+        ElseIf choice = "red" Then
+            Label1.Show()
+            MessageBox.Show("Ok, changing the color to red")
+            Label1.ForeColor = Color.Red
+        Else
+            MessageBox.Show("This is not a valid color")
+            Label1.Hide()
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As System.Object, e As System.EventArgs) Handles Button2.Click
+        Dim a As Integer
+        a = InputBox("Please enter a value of a number")
+        Select Case a
+            Case Is < 0
+                MessageBox.Show("Your number is negative")
+            Case 0
+                MessageBox.Show("Your number is zero")
+            Case 1 To 99
+                MessageBox.Show("Your number is a smaller positive number")
+            Case Is > 100
+                MessageBox.Show("Your number is a bigger positive number")
+        End Select
+    End Sub
+
+    Private Sub Button3_Click(sender As System.Object, e As System.EventArgs) Handles Button3.Click
+        Dim answer As String
+        answer = InputBox("Do you want to take the challenge?")
+        answer = answer.ToLower()
+        If answer = "yes" Then
+            Me.BackColor = DefaultBackColor
+            MessageBox.Show("Ok, lets begin.")
+            answer = InputBox("What is 2+3?")
+            If answer = "5" Then
+                MessageBox.Show("You are correct!")
+                answer = InputBox("Solve the equation: 2x+8=32")
+                If answer = "12" Then
+                    MessageBox.Show("You are correct, you win the challenge")
+                    Me.BackColor = Color.White
+                    Label1.Text = "Congratulations"
+                    Label1.Font = New Font("Microsoft Sans Serif", 40)
+                Else
+                    MessageBox.Show("You are wrong, the answer is 12. You lose.")
+                    Me.BackColor = Color.Black
+                End If
+            Else
+                MessageBox.Show("You are wrong, the answer was 5. You lose")
+                Me.BackColor = Color.Black
+            End If
+        Else
+            MessageBox.Show("Ok, dont be scared")
+        End If
+    End Sub
+
+    Private Sub Button4_Click(sender As System.Object, e As System.EventArgs) Handles Button4.Click
+        Dim answer As String
+        answer = InputBox("Which one do you want to change backcolor, fontsize, or forecolor?")
+        answer = answer.ToLower()
+        If answer = "backcolor" Then
+            Label1.Show()
+            MessageBox.Show("Okay, changing the backcolor")
+            Label1.BackColor = Color.GreenYellow
+        ElseIf answer = "forecolor" Then
+            Label1.Show()
+            MessageBox.Show("Okay, changing the forecolor")
+            Label1.ForeColor = Color.Aquamarine
+        ElseIf answer = "fontsize" Then
+            Label1.Show()
+            MessageBox.Show("Okay changing fontsize")
+            Label1.Font = New Font("Microsoft Sans Serif", 20)
+        Else
+            MessageBox.Show("That is okay")
+            Label1.Text = "Label1"
+        End If
+    End Sub
+
+    Private Sub Button5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button5.Click
+        Dim grade As Integer
+        grade = InputBox("Input your score")
+        Select Case grade
+            Case Is < 59
+                MessageBox.Show("Your grade is: F")
+            Case Is > 90
+                MessageBox.Show("Your grade is: A")
+            Case 80 To 89
+                MessageBox.Show("Your grade is: B")
+            Case 70 To 79
+                MessageBox.Show("Your grade is: C")
+            Case 60 To 69
+                MessageBox.Show("Your grade is: D")
+        End Select
+    End Sub
+
+    Private Sub Button6_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+
+    End Sub
+
+    Private Sub Label1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Button6_Click_1(sender As System.Object, e As System.EventArgs) Handles Button6.Click
+        Dim answer As String
+        answer = InputBox("Do you want to change the color to Red or Blue?")
+        answer = answer.ToLower()
+        If answer = "red" Then
+            MessageBox.Show("Okay, changing to red")
+            Button6.BackColor = Color.Red
+        ElseIf answer = "blue" Then
+            MessageBox.Show("Okay, changing to blue")
+            Button6.ForeColor = Color.Blue
+        Else
+            MessageBox.Show("That is okay")
+            Label1.Text = "Label1"
+        End If
+    End Sub
+End Class
---
+++ Decisions 2/Decisions 2/Form1.Designer.vb
@@ -0,0 +1,123 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(164, 129)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(108, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Color Choice"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(122, 87)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Label1"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(12, 129)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(108, 23)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Number Checker"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(164, 12)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(108, 23)
+        Me.Button3.TabIndex = 3
+        Me.Button3.Text = "Challenge"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(12, 12)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(108, 23)
+        Me.Button4.TabIndex = 4
+        Me.Button4.Text = "Your Choice"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(12, 227)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(108, 23)
+        Me.Button5.TabIndex = 5
+        Me.Button5.Text = "Score"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(164, 226)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(108, 23)
+        Me.Button6.TabIndex = 6
+        Me.Button6.Text = "Choose Color"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents Button6 As System.Windows.Forms.Button
+
+End Class
