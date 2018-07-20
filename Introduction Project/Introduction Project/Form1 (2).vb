0902b28c62c95e95b9be993fbe5fbf4bc378c010
ahassanein99 <achmadoo@outlook.com>
2018-07-20 01:13:48 -0400

    Add files via upload

---
+++ Introduction Project/Introduction Project/Form1 (2).vb
@@ -0,0 +1,24 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        MessageBox.Show("Welcome to VB")
+    End Sub
+
+    Private Sub Button2_Click(sender As System.Object, e As System.EventArgs) Handles Button2.Click
+        Dim username As String
+        username = "Bob"
+        username = InputBox("What is your name?")
+        MessageBox.Show(username)
+    End Sub
+
+    Private Sub Button3_Click(sender As System.Object, e As System.EventArgs) Handles Button3.Click
+        MessageBox.Show("This is my first Visual Basic Program")
+    End Sub
+
+    Private Sub Button4_Click(sender As System.Object, e As System.EventArgs) Handles Button4.Click
+        Dim fav As String
+        fav = "5"
+        fav = InputBox("What is your favortie number?")
+        MessageBox.Show(fav)
+    End Sub
+End Class
---
+++ Introduction Project/Introduction Project/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Introduction Project")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Wayne Board of Education")> 
+<Assembly: AssemblyProduct("Introduction Project")> 
+<Assembly: AssemblyCopyright("Copyright © Wayne Board of Education 2014")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("bc28a4c5-ab4d-4ada-94f8-f899765413e2")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
