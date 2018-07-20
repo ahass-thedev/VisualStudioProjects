476d98c4c50d3554fb52b0278451cadc6ec8dca1
ahassanein99 <achmadoo@outlook.com>
2018-07-20 01:03:11 -0400

    Add files via upload

---
+++ do loops/do loops/Form1 (2).vb
@@ -0,0 +1,45 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        Dim answer As String
+        Do
+            answer = InputBox("Do you want to see this message again")
+            answer = answer.ToLower()
+        Loop While (answer = "yes")
+    End Sub
+
+    Private Sub Button2_Click(sender As System.Object, e As System.EventArgs) Handles Button2.Click
+        Dim answer As Integer
+        answer = InputBox("Input a number.")
+        If answer > 100 Then
+            MessageBox.Show("Your number is greater than 100, no loop")
+        End If
+        Do Until (answer > 100)
+            answer = answer + 10
+            MessageBox.Show("Your new number is " & answer)
+        Loop
+    End Sub
+
+    Private Sub Button3_Click(sender As System.Object, e As System.EventArgs) Handles Button3.Click
+        Dim answer, question As Integer
+        answer = InputBox("what is 2+2?")
+        If answer = "4" Then
+            MessageBox.Show("True")
+        End If
+        Do Until (answer = "4")
+            answer = InputBox("what is 2+2?")
+            question = question + 1
+        Loop
+        MessageBox.Show("You got it correct, your total number of tries: " & question)
+    End Sub
+
+    Private Sub Button4_Click(sender As System.Object, e As System.EventArgs) Handles Button4.Click
+        Dim answer, count As Integer
+        count = 0
+        answer = InputBox("How many times would you like this to be repeated?")
+        Do Until answer = count
+            MessageBox.Show("Repeated " & count)
+            count = count + 1
+        Loop
+    End Sub
+End Class
---
+++ do loops/do loops/My Project/AssemblyInfo (2).vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("do loops")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Wayne Board of Education")> 
+<Assembly: AssemblyProduct("do loops")> 
+<Assembly: AssemblyCopyright("Copyright © Wayne Board of Education 2014")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("63369af3-189c-44bf-9401-3b58065e5c4b")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
